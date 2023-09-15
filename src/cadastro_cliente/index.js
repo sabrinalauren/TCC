db22a280a3f220fdb135eceb4991f807478fc941
isainfo <isadorinha15.dorinha@gmail.com>
2023-09-15 17:24:29 -0300

    ççç

--- src/cadastro_cliente/index.js
+++ src/cadastro_cliente/index.js
@@ -17,8 +17,8 @@ export default function cliente() {
         
       <div className="bordatrasparente">
         <div>
-          <button className='pessoafisica'> PESSOA FÍSICA</button>
-          <h1>*CAMPO OBRIGATÓRIO</h1>
+          <button className='pessoafisica'>PESSOA FÍSICA</button>
+          <h1>*CAMPO OBRIGATÓRIO </h1>
         </div>
 
         <MeuComponente />
